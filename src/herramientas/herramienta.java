a993b37c291ac9b275bc4660e05d35538abacab7
ivanojitos <ivanojitos@ivanojitos>
2016-05-13 14:40:44 -0500

    prueba

--- src/herramientas/herramienta.java
+++ src/herramientas/herramienta.java
@@ -4,6 +4,6 @@ public class herramienta {
 int date [] = {0,4,42};
 
 public void display(){
-	System.out.println("TODO: aplicacion visual de datos");
+	System.out.println("TODO: aplicacion visual de datos\n");
 }
 }
