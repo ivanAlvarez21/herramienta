77b46a894cc6b9e37d594621d53733a56ce9db00
ivanojitos <ivanojitos@ivanojitos>
2016-05-13 14:13:09 -0500

    la comprobacion inicial de herramientas de ventas

---
+++ src/herramientas/Main.java
@@ -0,0 +1,19 @@
+package herramientas;
+
+public class Main {
+
+	public static void main(String[] args) {
+	herramienta data = new herramienta();
+	
+	displayGreeting();
+	data.display();
+
+	}
+	
+	private static void displayGreeting(){
+		System.out.print("hola vendedores como estamos!\n");
+		System.out.print("esta aplicacion muestra los datos de venta\n");
+		
+	}
+
+}
---
+++ src/herramientas/herramienta.java
@@ -0,0 +1,9 @@
+package herramientas;
+
+public class herramienta {
+int date [] = {0,4,42};
+
+public void display(){
+	System.out.println("TODO: aplicacion visual de datos");
+}
+}
